87ab19aadb9f03892459774c7437f60db9b2a587
Xapsiel <111751721+Xapsiel@users.noreply.github.com>
2023-10-09 04:06:12 +0300

    Update main.cpp

--- contest_03/01/main.cpp
+++ contest_03/01/main.cpp
@@ -1,59 +1,24 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 
-
-bool is_digit(char elem) {
-	return 48 <= elem && elem <= 57;
-}
-
-
-void ReString(std::string& stack, std::string& output) {
-	while (stack[0] != '(' && !stack.empty()) {
-		output += " "+ std::string(1, stack[0])   ;
-		stack = stack.substr(1);
+std::vector<int> push_to_vector(int n, std::vector<int> all_numbers) {
+	for (int i=0; i < n; i++) {
+		int temp;
+		std::cin >> temp;
+		all_numbers.push_back(temp);
 	}
-	stack = stack.substr(1);
-	
+	return all_numbers;
 }
-
-int priority(char elem) {
-	if (elem == '^') return 3;
-	else if (elem == '/' || elem == '*' || elem =='%') return 2;
-	else if (elem == '-' || elem == '+') return 1;
-	return 0;
-}
-bool is_operator(char elem) {
-	return elem == '+' || elem == '-' || elem == '*' || elem == '/' || elem == '%'||elem=='^';
-}
- 
 int main() {
-
-	std::string expression;
-
-	std::cin >> expression;
-	std::string output = "";
-	std::string stack = "";
-	for (auto& elem : expression) {
-		std::string temp(1, elem);
-		if (temp == "(") stack = '(' + stack;
-		else if (temp == ")") ReString(stack, output);
-		else if (is_operator(elem)) {
-			output += " ";
-			while (!stack.empty() && priority(elem) <= priority(stack[0])) {
-				if (elem == stack[0] && elem == '^') break;
-
-				output +=  std::string(1, stack[0])+" ";
-				stack = stack.substr(1);
-			}
-			stack = temp + stack;
-		}
-		else if (is_digit(elem)) {
-			output += std::string(1, elem);
-		}
-	}
-	std::cout << output<<" ";
-	for (auto& c: stack){
-		std::cout << c << " ";
+	int n, m;
+ 	std::cin >> n;
+	std::vector<int> all_numbers;
+	all_numbers = push_to_vector(n, all_numbers);
+	std::cin >> m;
+	all_numbers = push_to_vector(m, all_numbers);
+	std::sort(all_numbers.begin(), all_numbers.end());
+	for (int elem : all_numbers) {
+		std::cout << elem << " ";
 	}
 }
- 
